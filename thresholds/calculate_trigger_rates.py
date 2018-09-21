d4116cb6d7824fd6321bf15a85fe6ef14004ad5f
cg-laser <jcglaser@gmail.com>
2018-09-21 00:21:09 +0000

    add function for single sided rates

--- thresholds/calculate_trigger_rates.py
+++ thresholds/calculate_trigger_rates.py
@@ -14,17 +14,26 @@ def get_global_trigger_rate(r_single, n_channels, n_coincidences, time_window):
     return n_coincidences * scipy.special.comb(n_channels, n_coincidences) * r_single**n_coincidences * time_window**(n_coincidences - 1)
 
 
-def get_sigma(rate, dt=1 * units.ns, time_window_high_low=5 * units.ns):
+def get_sigma_high_low(rate, dt=1 * units.ns, time_window_high_low=5 * units.ns):
+    """
+    calculates the high+low threshold for a given trigger rate
+    """
     def obj(sigma):
         return get_high_low_rate(sigma, dt=dt, time_window_high_low=time_window_high_low) - rate
     res = opt.brentq(obj, 0, 10)
     return res
 
+def get_sigma(rate, dt=1 * units.ns):
+    """
+    calculates the single sided threshold for a given trigger rate
+    """
+    return stats.norm.isf(rate * dt)
+
 
 def get_threshold(r_global, n_channels, n_coincidences, time_window, dt=1 * units.ns, time_window_high_low=5 * units.ns):
     r_single = (r_global / n_coincidences / scipy.special.comb(n_channels, n_coincidences) /
                 time_window**(n_coincidences - 1)) ** (1. / n_coincidences)
-    sigma = get_sigma(r_single, dt, time_window_high_low=time_window_high_low)
+    sigma = get_sigma_high_low(r_single, dt, time_window_high_low=time_window_high_low)
     print("global rate {:.0f} mHz {}/{}, {:.0f} ns -> {:.1f}".format(r_global / units.Hz / units.milli, n_coincidences,
                                                                    n_channels, time_window, sigma))
 
